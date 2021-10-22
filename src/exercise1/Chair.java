c0e6193a1941f7a7735bca1ecde58ab88c77b6a0
SidharthRang <sidrang.sr@gmail.com>
2021-10-22 16:09:50 -0700

    Second commit

--- src/exercise1/Chair.java
+++ src/exercise1/Chair.java
@@ -35,6 +35,30 @@ public class Chair {
         }
     }
 
+    public class Desk {
+        String material = "wood", shape = "rectangular", color = "Brown";
+        int height = 1, width = 1, length = 1, price = 25, NumOfLegs = 4;
+        boolean hasDrawer = true, isFoldable = true;
+    
+        public Desk(String material, boolean hasDrawer, boolean isFoldable, int price) {
+            this.material = material;
+            this.hasDrawer = hasDrawer;
+            this.isFoldable = isFoldable;
+            this.price = price;
+            System.out.println("The Desk is made of " + material + ". It has " + (hasDrawer ? "a drawer" : "no drawers")
+                    + " and is " + (isFoldable ? "foldable" : "not foldable"));
+        }
+    
+        public void OpenDrawer() {
+        }
+    
+        public void CloseDrawer() {
+        }
+    
+        public void Fold() {
+        }
+    }
+    
     public void AdjustHeight() {
     }
 
